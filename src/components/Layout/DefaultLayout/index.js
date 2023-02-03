9fd34c831da037fc91bba28a8e32dd0420bd411f
QuanVu <quan2082003@gmail.com>
2023-02-03 22:42:58 +0700

    layout

---
+++ src/components/Layout/DefaultLayout/index.js
@@ -0,0 +1,13 @@
+function DefaultLayout({ children }) {
+    return (
+        <div>
+            <Header />
+            <div className="container">
+                <SideBar />
+                <div className="content">{children}</div>
+            </div>
+        </div>
+    );
+}
+
+export default DefaultLayout;
